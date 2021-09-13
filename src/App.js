c6ae1fc0eba4d50f5380e4d1acbda81480bfc54d
Code-Victor <oluwaborihamzat@gmail.com>
2021-09-13 07:04:16 +0100

    layout complete

--- src/App.js
+++ src/App.js
@@ -13,28 +13,36 @@ const Accounts=  [
     Followers:'1987',
     days:12,
     increase:true,
-    icon:facebook
+    icon:facebook,
+    color:"blue-facebook",
+    gradient:false
   },
   {
     Name:'@nathanf',
     Followers:'1044',
     days: 99,
     increase:true,
-    icon:twitter
+    icon:twitter,
+    color:"blue-twitter",
+    gradient:false
   },
   {
     Name: '@realnathanf',
     Followers:'11k',
     days:1099,
     increase:true,
-    icon:instagram
+    icon:instagram,
+    color:["instagram-start","instagram-stop"],
+    gradient:true
   },
   {
     Name:'Nathan F.',
     Followers:'8239',
     days:144,
     increase:false,
-    icon:youtube
+    icon:youtube,
+    color:"red-youtube",
+    gradient:false
   },
   ]
 
@@ -102,7 +110,7 @@ const stats=[
 function App() {
   return (
     <>
-        <header className="pt-4 bg-white-top dark:bg-blue-top h-56 rounded-b-3xl ">
+        <header className="pt-4 bg-white-top dark:bg-blue-top h-56 rounded-b-3xl  ">
           <div className="flex justify-between flex-col md:flex-row md:items-center divide-y md:divide-none divide-gray-500 divide-solid  dark:text-white w-3/4 mx-auto ">
             <h1 className="flex flex-col font-extrabold  text-2xl mb-5 md:mb-0">Social Media Dashboard <span className=" text-sm font-bold text-gray-400">Total Followers 23,004</span></h1>
             <div className="flex text-gray-500 font-bold justify-between md:justify-between pt-5 md:pt-0 gap-2  ">
@@ -114,20 +122,23 @@ function App() {
           <section className="-mt-10 grid lg:grid-cols-4 md:grid-cols-2 gap-4 mb-4">
           {Accounts.map((element)=>{
             return(
-            <div className="px-6 py-4 bg-white-card dark:bg-blue-card dark:text-white flex items-center flex-col gap-y-6 rounded-lg">
-                <div className="flex items-center gap-1 font-bold">
-                  <img src={element.icon} alt=""  />
-                  <h3>{element.Name}</h3>
-                </div>
-                <h1 className="font-bold text-7xl flex flex-col items-center text-blue-dark dark:text-white">
-                  {element.Followers}
-                  <span className="text-gray-500 uppercase tracking-widest text-base">Followers</span>
-                </h1>
-                
-                <div className={`flex items-center text-sm ${element.increase?"text-green-500":"text-red-500"} font-bold gap-1`}>
-                  {element.increase?<UpLogo/>:<DownLogo/>}
-                  {element.days} Today
-                </div>
+            <div className="">
+              <div className={"inline-block w-full h-2 rounded-t-lg -mb-2 "+`${element.gradient? "bg-gradient-to-r"+" "+"from-"+element.color[0]+" "+"to-"+element.color[1]:"bg-"+element.color}`}></div>
+              <div className="px-6 py-4 bg-white-card dark:bg-blue-card dark:text-white flex items-center flex-col gap-y-6 rounded-lg">
+                  <div className="flex items-center gap-1 font-bold">
+                    <img src={element.icon} alt=""  />
+                    <h3>{element.Name}</h3>
+                  </div>
+                  <h1 className="font-bold text-7xl flex flex-col items-center text-blue-dark dark:text-white">
+                    {element.Followers}
+                    <span className="text-gray-500 uppercase tracking-widest text-base">Followers</span>
+                  </h1>
+              
+                  <div className={`flex items-center text-sm ${element.increase?"text-green-500":"text-red-500"} font-bold gap-1`}>
+                    {element.increase?<UpLogo/>:<DownLogo/>}
+                    {element.days} Today
+                  </div>
+              </div>
             </div>
             )
           }) } 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
        "blue-card":"#252a41",
        "white-top":"#f5f7ff",
        "white-card":"#f0f2fa",
+       "blue-facebook":"hsl(208, 92%, 53%)",
+       "blue-twitter":"hsl(203, 89%, 53%)",
+       "instagram-start":"hsl(37, 97%, 70%)",
+       "instagram-stop":"hsl(329, 70%, 58%)",
+       "red-youtube":"hsl(348, 97%, 39%)"
       }
     },
   },
